b1ff558fb138c5554d06d177a9e59d3458cc33c8
Agustin Ituarte <agustin242536@gmail.com>
2022-08-07 17:31:21 -0300

    Functional Login

---
+++ Functional Login/login.js
@@ -0,0 +1,68 @@
+let user = ["miguel", "pepe",];
+let passwd = ["123", "@1234"];
+
+function validate()
+{
+    var username = document.getElementById("usuario").value;
+    var password = document.getElementById("contraseña").value;
+	var user_logged = false
+       user.forEach(function(item, i, arr){
+           if (user[i] == username && passwd[i] == password){
+               user_logged = true
+           }      
+       })
+	   if (user_logged == true)
+	   {
+		alert("Usuario logueado.");   
+        localStorage.setItem('nombre', username);
+        localStorage.setItem('contraseña', password);
+        window.location.href = 'bienvenida.html';
+	   }
+	   else
+       {
+		alert("Usuario incorrecto.");
+       }
+}
+
+function register_user()
+{
+    var username = document.getElementById("usuario").value;
+    var password = document.getElementById("contraseña").value;
+    user.push(username);
+    passwd.push(password);
+    alert("Registro completo");
+}
+
+
+function reset_passwd()
+{
+    var username = document.getElementById("usuario").value;
+    var password = document.getElementById("contraseña").value;
+    var new_password = document.getElementById("nueva_contraseña").value;
+   
+       user.forEach(function(item, i, arr){
+           if (user[i] == username && passwd[i] == password){
+               passwd[i] = new_password;
+               alert("Nueva contraseña : " + passwd[i]);
+           }
+       })
+}
+
+function delete_user()
+{
+    var username = document.getElementById("usuario").value;
+    var password = document.getElementById("contraseña").value;
+   
+       user.forEach(function(item, i, arr){
+           if (user[i] == username && passwd[i] == password){
+               delete user[i];
+               delete passwd[i];
+               alert("El usuario ha sido eliminado.");
+           }
+       })
+}
+
+function load_data()
+{
+    document.getElementById('usuario').innerHTML = localStorage.getItem('nombre');
+}
